0bbd5c536f5f1f3c2763aaa931d14bf0645c09e6
Javeed9888 <ansarsheikh9080@gmail.com>
2025-03-04 14:46:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+module.exports = async function (context, req) {
+    context.log("JavaScript HTTP trigger function processed a request.");
+    context.res = {
+        status: 200,
+        body: "Hello from Azure Function!"
+    };
+};
 
